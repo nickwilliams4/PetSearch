d9f810fc07afab470b3669d06d72e3bf0efafbcd
nickwilliams4 <50678575+nickwilliams4@users.noreply.github.com>
2019-11-20 10:42:30 -0700

    load species function

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
 'use strict';
-const zipCode = $('.zipSearch').val();
-const radius = $('.radiusValue').val();
-const typeOf = $('.type').val();
 
-function petSearch(zipCode, radius) {
+const store = {
+  species: [],
+  colors: []
+};
+function loadSpecies() {
+  const url = "https://api.rescuegroups.org/http/v2.json";
+  const data = {
+    apikey: "PlqQjhlx",
+    objectType: "animalSpecies",
+    objectAction: "publicList"
+  };
+  try {
+    const response = fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then(responseJson => {
+        store.species = responseJson.data;
+        const speciesList = Object.keys(store.species)
+          .map(species => `<option value="${species}">${species}</option>`)
+          .join("");
+        $(".type").html(speciesList);
+      });
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+function petSearch(zipCode, radius, typeOf) {
   const url = 'https://api.rescuegroups.org/http/v2.json';
 const data = { "apikey": "PlqQjhlx", "objectType": "animals", "objectAction": "publicSearch",  "search":
 {
     "resultStart": "0",
-    "resultLimit": "10",
+    "resultLimit": "20",
     "resultSort": "animalID",
     "resultOrder": "asc",
     "filters":
@@ -27,12 +61,17 @@ const data = { "apikey": "PlqQjhlx", "objectType": "animals", "objectAction": "p
             "fieldName": "animalLocation",
             "operation": "equals",
             "criteria": `${zipCode}`
-        }
+        },
+        {
+          "fieldName": "animalSpecies",
+          "operation": "equals",
+          "criteria": `${typeOf}`
+      },
     ],
     "filterProcessing": "1",
     "fields":
     [
-        "animalID","animalOrgID","animalActivityLevel","animalAdoptedDate","animalAdoptionFee","animalAgeString","animalAltered","animalAvailableDate","animalBirthdate","animalBirthdateExact","animalBreed","animalCoatLength","animalColor","animalColorID","animalColorDetails","animalCourtesy","animalDeclawed","animalDescription","animalDescriptionPlain","animalDistinguishingMarks","animalEarType","animalEnergyLevel","animalExerciseNeeds","animalEyeColor","animalFence","animalFound","animalFoundDate","animalFoundPostalcode","animalGeneralAge","animalGeneralSizePotential","animalGroomingNeeds","animalHousetrained","animalIndoorOutdoor","animalKillDate","animalKillReason","animalLocation","animalLocationCoordinates","animalLocationDistance","animalLocationCitystate","animalMicrochipped","animalMixedBreed","animalName","animalSpecialneeds","animalSpecialneedsDescription","animalNeedsFoster","animalNewPeople","animalNotHousetrainedReason","animalObedienceTraining","animalOKWithAdults","animalOKWithCats","animalOKWithDogs","animalOKWithKids","animalOwnerExperience","animalPattern","animalPatternID","animalAdoptionPending","animalPrimaryBreed","animalPrimaryBreedID","animalRescueID","animalSearchString","animalSecondaryBreed","animalSecondaryBreedID","animalSex","animalShedding","animalSizeCurrent","animalSizePotential","animalSizeUOM","animalSpecies","animalSpeciesID","animalSponsorable","animalSponsors","animalSponsorshipDetails","animalSponsorshipMinimum","animalStatus","animalStatusID","animalSummary","animalTailType","animalThumbnailUrl","animalUptodate","animalUpdatedDate","animalUrl","animalVocal","animalYardRequired","animalAffectionate","animalApartment","animalCratetrained","animalDrools","animalEagerToPlease","animalEscapes","animalEventempered","animalFetches","animalGentle","animalGoodInCar","animalGoofy","animalHasAllergies","animalHearingImpaired","animalHypoallergenic","animalIndependent","animalIntelligent","animalLap","animalLeashtrained","animalNeedsCompanionAnimal","animalNoCold","animalNoFemaleDogs","animalNoHeat","animalNoLargeDogs","animalNoMaleDogs","animalNoSmallDogs","animalObedient","animalOKForSeniors","animalOKWithFarmAnimals","animalOlderKidsOnly","animalOngoingMedical","animalPlayful","animalPlaysToys","animalPredatory","animalProtective","animalSightImpaired","animalSkittish","animalSpecialDiet","animalSwims","animalTimid","fosterEmail","fosterFirstname","fosterLastname","fosterName","fosterPhoneCell","fosterPhoneHome","fosterSalutation","locationAddress","locationCity","locationCountry","locationUrl","locationName","locationPhone","locationState","locationPostalcode","animalPictures","animalVideos","animalVideoUrls"
+      "animalSpecies","animalOrgID","animalActivityLevel","animalAdoptedDate","animalAdoptionFee","animalAgeString","animalAltered","animalAvailableDate","animalBirthdate","animalBirthdateExact","animalBreed","animalCoatLength","animalColor","animalColorID","animalColorDetails","animalCourtesy","animalDeclawed","animalDescription","animalDescriptionPlain","animalDistinguishingMarks","animalEarType","animalEnergyLevel","animalExerciseNeeds","animalEyeColor","animalFence","animalFound","animalFoundDate","animalFoundPostalcode","animalGeneralAge","animalGeneralSizePotential","animalGroomingNeeds","animalHousetrained","animalIndoorOutdoor","animalKillDate","animalKillReason","animalLocation","animalLocationCoordinates","animalLocationDistance","animalLocationCitystate","animalMicrochipped","animalMixedBreed","animalName","animalSpecialneeds","animalSpecialneedsDescription","animalNeedsFoster","animalNewPeople","animalNotHousetrainedReason","animalObedienceTraining","animalOKWithAdults","animalOKWithCats","animalOKWithDogs","animalOKWithKids","animalOwnerExperience","animalPattern","animalPatternID","animalAdoptionPending","animalPrimaryBreed","animalPrimaryBreedID","animalRescueID","animalSearchString","animalSecondaryBreed","animalSecondaryBreedID","animalSex","animalShedding","animalSizeCurrent","animalSizePotential","animalSizeUOM","animalSpecies","animalSpeciesID","animalSponsorable","animalSponsors","animalSponsorshipDetails","animalSponsorshipMinimum","animalStatus","animalStatusID","animalSummary","animalTailType","animalThumbnailUrl","animalUptodate","animalUpdatedDate","animalUrl","animalVocal","animalYardRequired","animalAffectionate","animalApartment","animalCratetrained","animalDrools","animalEagerToPlease","animalEscapes","animalEventempered","animalFetches","animalGentle","animalGoodInCar","animalGoofy","animalHasAllergies","animalHearingImpaired","animalHypoallergenic","animalIndependent","animalIntelligent","animalLap","animalLeashtrained","animalNeedsCompanionAnimal","animalNoCold","animalNoFemaleDogs","animalNoHeat","animalNoLargeDogs","animalNoMaleDogs","animalNoSmallDogs","animalObedient","animalOKForSeniors","animalOKWithFarmAnimals","animalOlderKidsOnly","animalOngoingMedical","animalPlayful","animalPlaysToys","animalPredatory","animalProtective","animalSightImpaired","animalSkittish","animalSpecialDiet","animalSwims","animalTimid","fosterEmail","fosterFirstname","fosterLastname","fosterName","fosterPhoneCell","fosterPhoneHome","fosterSalutation","locationAddress","locationCity","locationCountry","locationUrl","locationName","locationPhone","locationState","locationPostalcode","animalPictures","animalVideos","animalVideoUrls"
     ]
      
 } }
@@ -51,14 +90,24 @@ const data = { "apikey": "PlqQjhlx", "objectType": "animals", "objectAction": "p
       return response.json();
     })
     .then(responseJson => 
-      console.log(responseJson));
-  } catch (error) {
+      displayResults(responseJson))
+    } catch (error) {
     console.error('Error:', error);
   }
 }
 
+function displayResults(results) {
+  console.log(results);
+  $('.searchResults').html(results.data)
+  if (results.total == 0) {
+    $('.error-message').html('No results. Please try your search again.')
+  }
+}
 
 function searchHandler() {
+  const zipCode = $('.zipSearch').val();
+  const radius = $('.radiusValue').val();
+  const typeOf = $('.type').val();
   petSearch(zipCode, radius, typeOf);
 }
 
@@ -68,5 +117,5 @@ function watchForm() {
     searchHandler();
   });
 }
-
+loadSpecies();
 $(watchForm);
